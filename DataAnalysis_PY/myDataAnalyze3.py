b53a261e7b931aa9e2a8c8a47cb44d52472b9d1b
Cezar - Ubuntu 12.04 <cezar.suteu@gmail.com>
2016-01-06 13:44:38 +0100

    Added ICP routines

---
+++ DataAnalysis_PY/myDataAnalyze3.py
@@ -0,0 +1,269 @@
+
+#!/usr/bin/python
+#Data analysis
+import sys
+import os
+from myGnu2DCartesian import myGnu2DCartesian,myGnu2DCartesianMakeEmpty
+from myICP_Consecutive import myICP_Consecutive,myICP_Consecutive_MakeEmpty
+import datetime
+
+
+myStopAfter=True
+myStartOffset=10
+myStopAfterSeconds=3
+myBackGroundSampleIndex=3#target date to extract background features
+
+def mySend2GnuPlot(vCmd):
+  #print vCmd
+  myGnuFile.write(vCmd)
+
+if len(sys.argv)!=3:
+  print "Usage myDataAnalyze3.py fileToAnalyze [png/gif]"
+  sys.exit()
+else:
+  #check if dat folder exists in program path
+  if os.path.exists("dat"):
+    os.popen('rm -rf dat')
+  os.popen('mkdir dat')
+  myGnuTargetFilename='myPlotGP_ICP_CMD.gnu'
+  myGnuFile = open(myGnuTargetFilename, 'w')
+  print "DataAnalyze3 - output is "+str(myGnuTargetFilename)+". Launch gnuplot after!"
+  if str(sys.argv[2])=='png':
+    print "png output"
+    #check if dat folder exists in program path
+    if os.path.exists("png"):
+      os.popen('rm -rf png')
+    os.popen('mkdir png')
+    mySend2GnuPlot('set terminal png size 1200,800\n')
+  elif str(sys.argv[2])=='gif':
+    print "gif output"
+    mySend2GnuPlot('set terminal gif size 1200,800 animate delay 10\n')
+    #proc.stdin.write('set terminal gif size 1080,720 animate delay 100\n')
+    mySend2GnuPlot('set output \'myAnimation.gif\'\n')
+  else:
+    print "No output specified, please choose x11 or png"
+    sys.exit()
+  print "Finding "+sys.argv[1]+".."
+  if os.path.isfile(sys.argv[1]):
+    print "Opening "+sys.argv[1]+".."
+    with open(sys.argv[1]) as f:
+      lines = f.readlines()
+      myDistinctDates=[]
+      myTopicsNames=[]
+      myTopicsPose=[]
+      myPoints=[]
+      myPointsContainer=[]
+      myFirstData2DDone=[]
+      myBackGroundLL=[]
+      myMaxRange=0
+      #myTopicName="N_A"
+      print "Contains "+str(len(lines))+" lines"
+      for line in lines:
+	data=line.split()
+	if str(data[0])=="#":
+    #special control information, do not parse normally
+	  if str(data[1])=="topic":
+	    #general topic information, descriptor not needed
+	    myTopicsNames.append(data[2])
+	    myFirstData2DDone.append(False)
+	    myTempPoints=[]
+	    myPoints.append(myTempPoints)
+	    myLocalTopicsPose=[]
+	    for k in range(3,3+3):
+	      #Add translate X,Y, rotation
+	      myLocalTopicsPose.append(str(data[k]))
+	    myTopicsPose.append(myLocalTopicsPose)
+	  elif str(data[1])=="sum":
+	    if len(myPointsContainer)>0:
+	      myPoints[myTarget].append(myPointsContainer[:])
+	    #sum is an agregate of data which follows, switch target topic entry
+	    try: myTarget=myTopicsNames.index(data[2]);
+	    except ValueError: print "Topic "+str(data[2])+" not defined in topic statement"
+	    #print "Found sum of topic "+str(data[2])+", putting data to index "+str(myTarget)
+	    myPointsContainer=[]
+	    myPointsContainer.append(data[5])#initialise point container
+	    try: myDistinctDates.index(data[5]);
+	    except ValueError: myDistinctDates.append(data[5])
+	else:
+	  #on main data structure, myTarget is set
+	  myPointsContainer.append(data[0])#angle
+	  myPointsContainer.append(data[1])#range
+	  if data[1]>myMaxRange:
+	    myMaxRange=data[1]
+    for i in range(0,len(myTopicsNames)):
+      print "Topic "+str(myTopicsNames[i])+" has "+str(len(myPoints[i]))+" elements(incl date as point)"
+    print "Total "+str(len(myDistinctDates))+" independent instances of seconds"
+    #sort dates just in case
+    myDistinctDates.sort()
+    
+    #obtain background sample
+    myBackGroundDate=myDistinctDates[myBackGroundSampleIndex];
+    print "Background date:"+myBackGroundDate
+    myBackgroundContainer=[]
+    for i in range(0,len(myTopicsNames)):
+      myLocalPointContainer=[]
+      for myPoint in myPoints[i]:
+	if myBackGroundDate==myPoint[0]:#match the current dates
+	  myLocalPointContainer.append(myPoint)
+      myBackgroundContainer.append(myLocalPointContainer[0])
+      print "Topic "+myTopicsNames[i]+" has "+str(len(myBackgroundContainer[i]))+" ranges(angle+distance) +1 date in background sample"
+	  
+    #Preamble
+    myMaxRange=2.5
+    mySend2GnuPlot('myCartesianXStart='+str(myMaxRange)+'\n')
+    mySend2GnuPlot('myCartesianXEnd='+str(myMaxRange)+'\n')
+    mySend2GnuPlot('myCartesianYStart='+str(myMaxRange)+'\n')
+    mySend2GnuPlot('myCartesianYEnd='+str(myMaxRange)+'\n')
+    mySend2GnuPlot('set xzeroaxis\n')
+    mySend2GnuPlot('set yzeroaxis\n')
+    mySend2GnuPlot('set rmargin 1\n')
+    mySend2GnuPlot('set lmargin 3\n')
+    mySend2GnuPlot('set tmargin 2\n')
+    mySend2GnuPlot('set bmargin 2\n')
+    mySend2GnuPlot('set xrange [-myCartesianXStart:myCartesianXEnd]\n')
+    mySend2GnuPlot('set yrange [-myCartesianYStart:myCartesianYEnd]\n')
+    mySend2GnuPlot('set size square\n')
+    mySend2GnuPlot('set key right bottom\n')
+    myFileOutput=0
+    myICP_PreviousDataSet=[]
+    myEmptyList=[]
+    myICP_PreviousDataSet.append(myEmptyList)
+    myICP_PreviousDataSet.append(myEmptyList)
+    myICP_PreviousDataSet.append(myEmptyList)
+    for myDate in myDistinctDates:
+      DoPlot=False
+      if myStopAfter:
+	if myDistinctDates.index(myDate)>=myStartOffset:
+	  DoPlot=True
+	if myDistinctDates.index(myDate)>myStopAfterSeconds+myStartOffset:
+	  break
+      else:
+	DoPlot=True
+      #print myDate
+      if DoPlot:
+	#create a list of lists for the robot points
+	myLocalLL=[]
+	for i in range(0,len(myTopicsNames)):
+	  myLocalPointContainer=[]
+	  for myPoint in myPoints[i]:
+	    if myDate==myPoint[0]:#match the current dates
+	      myLocalPointContainer.append(myPoint)
+	  myLocalLL.append(myLocalPointContainer)#save the list which matches the current date from certain topic
+	print "Date "+str(myDate)+" has:"
+	for i in range(0,len(myTopicsNames)):
+	  print myTopicsNames[i]+" "+str(len(myLocalLL[i]))+" hits."
+	#Gnuplot code creation: myLocalLL contains lists of myPoint, generate image code in gnuplot
+
+	#Start on step 0
+	myFoundAnyPointImage=False
+
+	for i in range(0,len(myTopicsNames)):
+	  myFoundAnyPointImage=myFoundAnyPointImage or len(myLocalLL[i])>0
+	myStartStep=0
+	while myFoundAnyPointImage:
+
+	  #print "Sequence "+str(myStartStep)
+	  myFoundAnyPointImage=False
+	  myGnuPlotOutput=[]
+	  myGnuPlotOutput2=[]
+	  #first determine if there is still any image on current level
+	  for i in range(0,len(myTopicsNames)):
+	    if len(myLocalLL[i])>=myStartStep+1:
+	      myFoundAnyPointImage=True
+	  if myFoundAnyPointImage:
+	    myAgregateGnuCmds=[]
+	    #myAgregateGnuCmds.append('unset multiplot\n')
+	    #myAgregateGnuCmds.append('set bmargin at screen 0.1\n')
+	    myAgregateGnuCmds.append('set title "Aggregated with pose"\n')
+	    myAgregateGnuCmds.append('plot ')
+	    myAgregatePointList=[]
+	    if str(sys.argv[2])=='png':
+	      #mySend2GnuPlot('set output \'png/file'+'%5s' % myFileOutput+'.png\'\n')
+	      mySend2GnuPlot('set output \'png/file'+str(myFileOutput).zfill(5)+'.png\'\n')
+	      myFileOutput=myFileOutput+1
+	    #mySend2GnuPlot('set multiplot layout 2,1\n')
+	    mySend2GnuPlot('set multiplot layout 2,'+str(len(myTopicsNames))+'\n')
+	    for i in range(0,len(myTopicsNames)):
+	      #print str(len(myLocalLL[i]))+">="+str(myStartStep)
+	      myQualifiedTopicName=myTopicsNames[i]+"_"+str(myDate)+"_"+str(myStartStep)
+	      myLocalGnuCmds=[]
+	      myLocalGnuCmds2=[]
+	      myLocalGnuCmds3=[]
+	      if (i==0):
+		myLocalGnuCmds2.append('set xrange [-myCartesianXStart:myCartesianXEnd]\n')
+		myLocalGnuCmds2.append('set yrange [-myCartesianYStart:myCartesianYEnd]\n')
+	      if (i==1):
+		myDateTimeObj=datetime.datetime.fromtimestamp(float(myDate))
+		myDateTimeObjBack=datetime.datetime.fromtimestamp(float(myBackGroundDate))
+		#myLocalGnuCmds.append('set title "'+str(myDateTimeObj.strftime("%Y-%m-%d %H:%M:%S"))+'"\n')
+		myLocalGnuCmds2.append('set title "'+str(myDateTimeObj.strftime("%Y-%m-%d %H:%M:%S"))+' with background '+str(myDateTimeObjBack.strftime("%Y-%m-%d %H:%M:%S"))+'"\n')
+		myLocalGnuCmds3.append('set title "ICP matching"\n')
+	      else:
+		myLocalGnuCmds.append('set title "-"\n')
+		myLocalGnuCmds2.append('set title "-"\n')
+		myLocalGnuCmds3.append('set title "-"\n')
+	      if len(myLocalLL[i])>=myStartStep+1:
+		myResult=myGnu2DCartesian(myLocalLL[i][myStartStep],myQualifiedTopicName,myDate,myTopicsPose[i],myBackgroundContainer[i])
+		myLocalGnuCmds.append(myResult[0])
+		myAgregateGnuCmds.append(myResult[1])
+		#myLocalGnuCmds2.append(myResult[2])
+		if len(myICP_PreviousDataSet[i])>0:
+		  myResult_ICP=myICP_Consecutive(myLocalLL[i][myStartStep],myICP_PreviousDataSet[i],myQualifiedTopicName,myDate,myTopicsPose[i],myBackgroundContainer[i])
+		  #myLocalGnuCmds3.append(myResult_ICP[2])
+		  #myDynamicRangeXStart=-0.5
+		  #myDynamicRangeXEnd=-0.3
+		  #myDynamicRangeYStart=-0.5
+		  #myDynamicRangeYEnd=-0.3
+		  #myLocalGnuCmds2.append('set xrange ['+str(myDynamicRangeXStart)+':'+str(myDynamicRangeXEnd)+']\n')
+		  #myLocalGnuCmds2.append('set yrange ['+str(myDynamicRangeYStart)+':'+str(myDynamicRangeYEnd)+']\n')
+		  myLocalGnuCmds2.append(myResult_ICP[2])
+		  myLocalGnuCmds3.append(myResult_ICP[3])
+		  myLocalGnuCmds3.append(myResult_ICP[4])
+		  myLocalGnuCmds3.append(myResult_ICP[5])
+		  myLocalGnuCmds3.append(myResult_ICP[6])
+		  myLocalGnuCmds3.append(myResult_ICP[7])
+		else:
+		  myResult_ICP=myICP_Consecutive_MakeEmpty(myQualifiedTopicName)
+		  myLocalGnuCmds2.append(myResult[2])
+		  myLocalGnuCmds3.append(myResult_ICP[0])
+		myICP_PreviousDataSet[i]=myLocalLL[i][myStartStep]
+		myFoundAnyPointImage=True
+		#print len(myGnuPlotOutput)
+	      else:
+		#this topic does not have hits, just put empty
+		myResult=myGnu2DCartesianMakeEmpty(myQualifiedTopicName)
+		myLocalGnuCmds.append(myResult[0])
+		myAgregateGnuCmds.append(myResult[1])
+		#use same single empty data in case there is no points
+		myLocalGnuCmds2.append(myResult[0])
+		myResult_ICP=myICP_Consecutive_MakeEmpty(myQualifiedTopicName)
+		myLocalGnuCmds3.append(myResult_ICP[0])
+		myAgregateGnuCmds.append(',')
+		#myAgregatePointList+=myGnu2DCartesianMakeEmpty("/LMS_agregated")
+	      #get a command list for combined graph
+	      #myAgregateGnuCmds+=myGnu2DCartesian(myAgregatePointList,"/LMS_agregated",myDate)
+	      #do not plot anymore the complete picture
+	      #myGnuPlotOutput+=myLocalGnuCmds
+	      #myLocalGnuCmds2 is subtracted image
+	      myGnuPlotOutput+=myLocalGnuCmds2
+	      #myLocalGnuCmds3 is result of ICP algorithm
+	      myGnuPlotOutput2+=myLocalGnuCmds3
+	    #mySend2GnuPlot('plot ')
+	    #Use below for aggregate points
+	    #myGnuPlotOutput+=myAgregateGnuCmds
+	    #myGnuPlotOutput.append('\n')
+
+	    #get a plot command for all the points
+
+	    for i in range(0,len(myGnuPlotOutput)):
+	      mySend2GnuPlot(myGnuPlotOutput[i])
+	    for i in range(0,len(myGnuPlotOutput2)):
+	      mySend2GnuPlot(myGnuPlotOutput2[i])
+	    mySend2GnuPlot('unset multiplot\n')
+	    if str(sys.argv[2])=='png':
+	      mySend2GnuPlot('set output\n')
+	  myStartStep+=1
+  else:
+    print "Input file not found!"
+  myGnuFile.close()
+
---
+++ DataAnalysis_PY/myICP_Consecutive.py
@@ -0,0 +1,170 @@
+import math
+import datetime
+myGnuPlotCmds=[]
+myDebug=False
+mySubtractThreshhold=0.3
+myZoom=0.5
+myMinX=-1
+myMaxX=0
+myMinY=0
+myMaxY=1
+
+def myOutput(vMessage):
+  myGnuPlotCmds.append(vMessage)
+
+def myCreateDatFile(dataSeriesRaw,vFileName,vTranslateX,vTranslateY,vRotate):
+  global myDebug
+  #myTargetFilename='dat/output_'+vFileName+'.dat'
+  if myDebug:print "myCreateDatFile : ["+vFileName+"] Translate X "+str(vTranslateX)+" translate Y "+str(vTranslateY)+" rotate Z "+str(vRotate)
+  f = open(vFileName, 'w')
+  for i in range(1,len(dataSeriesRaw),2):
+    #checking point angle and range
+    myX=math.cos(math.radians(float(dataSeriesRaw[i])/2+vRotate))*float(dataSeriesRaw[i+1])+vTranslateX
+    myY=math.sin(math.radians(float(dataSeriesRaw[i])/2+vRotate))*float(dataSeriesRaw[i+1])+vTranslateY
+    #if myDebug:print "Angle "+str(float(dataSeriesRaw[i])/2)+" range "+str(dataSeriesRaw[i+1])+"=> X ["+str(myX)+"] Y ["+str(myY)+"]"
+    f.write(str(myX)+" "+str(myY)+"\n")
+  f.close()
+def myGetCentroid(dataSeriesRaw):
+  myResult=[]
+  myXSum=0
+  myYSum=0
+  myElements=0
+  for i in range(1,len(dataSeriesRaw),2):
+    myElements=myElements+1
+    myX=math.cos(math.radians(float(dataSeriesRaw[i])/2))*float(dataSeriesRaw[i+1])
+    myY=math.sin(math.radians(float(dataSeriesRaw[i])/2))*float(dataSeriesRaw[i+1])
+    myXSum=myXSum+myX
+    myYSum=myYSum+myY
+  if myElements>0:  
+    myResult.append(myXSum/myElements)
+    myResult.append(myYSum/myElements)
+  else:
+    myResult.append(0)
+    myResult.append(0)
+  return myResult
+def mySubtractBackground(dataSeriesRaw,dataSeriesBackGround):
+  myDataSeriesSubtracted=[]
+  myDataSeriesSubtracted.append(dataSeriesRaw[0])
+  for i in range(1,len(dataSeriesRaw),2):
+    if math.fabs(float(dataSeriesBackGround[i+1])-float(dataSeriesRaw[i+1]))>mySubtractThreshhold:
+      myDataSeriesSubtracted.append(dataSeriesRaw[i])
+      myDataSeriesSubtracted.append(dataSeriesRaw[i+1])
+  return myDataSeriesSubtracted
+def myFilterRange(dataSeriesRaw):
+  myDataSeriesSubtracted=[]
+  myDataSeriesSubtracted.append(dataSeriesRaw[0])
+  #print "myFilterRange"
+
+  for i in range(1,len(dataSeriesRaw),2):
+    myX=math.cos(math.radians(float(dataSeriesRaw[i])/2))*float(dataSeriesRaw[i+1])
+    myY=math.sin(math.radians(float(dataSeriesRaw[i])/2))*float(dataSeriesRaw[i+1])
+    if (myX>myMinX and myX<myMaxX and myY>myMinY and myY<myMaxY):
+      #print "Writing "+str(myX)+" "+str(myY)
+      myDataSeriesSubtracted.append(dataSeriesRaw[i])
+      myDataSeriesSubtracted.append(dataSeriesRaw[i+1])
+  return myDataSeriesSubtracted
+  
+def myCreateDatFileICP(dataSeriesRaw,vFileName,vTranslateX,vTranslateY,vRotate,dataSeriesPrevious):
+  global myDebug
+  #myTargetFilename='dat/output_'+vFileName+'.dat'
+  if myDebug:print "myCreateDatFileSample : ["+vFileName+"] Translate X "+str(vTranslateX)+" translate Y "+str(vTranslateY)+" rotate Z "+str(vRotate)
+  f = open(vFileName, 'w')
+  for i in range(1,len(dataSeriesRaw),2):
+    myDist=10000
+    myX=math.cos(math.radians(float(dataSeriesRaw[i])/2))*float(dataSeriesRaw[i+1])
+    myY=math.sin(math.radians(float(dataSeriesRaw[i])/2))*float(dataSeriesRaw[i+1])
+    myLastXPrev=10000
+    myLastYPrev=10000
+    for j in range(1,len(dataSeriesPrevious),2):
+      myXPrev=math.cos(math.radians(float(dataSeriesPrevious[j])/2))*float(dataSeriesPrevious[j+1])
+      myYPrev=math.sin(math.radians(float(dataSeriesPrevious[j])/2))*float(dataSeriesPrevious[j+1])
+      #calculate cartesian distance
+      myCartesianDistance=(myX-myXPrev)*(myX-myXPrev)+(myY-myYPrev)*(myY-myYPrev)
+      if myCartesianDistance<myDist:
+	myDist=myCartesianDistance
+	myLastXPrev=myXPrev
+	myLastYPrev=myYPrev
+    f.write(str(myX)+" "+str(myY)+"\n"+str(myLastXPrev)+" "+str(myLastYPrev)+"\n\n")
+  f.close()
+def myCreateDatFileSample(dataSeriesRaw,vFileName,vTranslateX,vTranslateY,vRotate,dataSeriesBackGround):
+  global myDebug
+  #myTargetFilename='dat/output_'+vFileName+'.dat'
+  if myDebug:print "myCreateDatFileSample : ["+vFileName+"] Translate X "+str(vTranslateX)+" translate Y "+str(vTranslateY)+" rotate Z "+str(vRotate)
+  f = open(vFileName, 'w')
+  for i in range(1,len(dataSeriesRaw),2):
+    if math.fabs(float(dataSeriesBackGround[i+1])-float(dataSeriesRaw[i+1]))>mySubtractThreshhold:
+      #checking point angle and range
+      myX=math.cos(math.radians(float(dataSeriesRaw[i])/2+vRotate))*float(dataSeriesRaw[i+1])+vTranslateX
+      myY=math.sin(math.radians(float(dataSeriesRaw[i])/2+vRotate))*float(dataSeriesRaw[i+1])+vTranslateY
+      #if myDebug:print "Angle "+str(float(dataSeriesRaw[i])/2)+" range "+str(dataSeriesRaw[i+1])+"=> X ["+str(myX)+"] Y ["+str(myY)+"]"
+      f.write(str(myX)+" "+str(myY)+"\n")
+  f.close()
+def myICP_Consecutive(dataSeriesRaw,dataSeriesPrevious,vTopicName,vDateTime,vTopicPose,dataSeriesBackGround):
+  myGnuPlotCmds=[]
+  myDebug=False
+  #make two passes, one with original coordinates, other with transformations
+  #original
+  myRotate=0#degrees rotate the robot angle, for plotting only
+  myTranslateX=0.0
+  myTranslateY=0.0
+  myTargetFilename='dat/output_'+vTopicName[1:]+'_ICP.dat'
+  if myDebug:print 'Target filename:'+myTargetFilename
+  myCreateDatFile(dataSeriesRaw,myTargetFilename,myTranslateX,myTranslateY,myRotate)
+  myGnuPlotCmds.append('plot "'+myTargetFilename+'" u 1:2 lt rgb "red" title "'+vTopicName[5:9]+'"\n')
+  #rotated
+  myTranslateX=float(vTopicPose[0])
+  myTranslateY=float(vTopicPose[1])
+  myRotate=float(vTopicPose[2])
+  myTargetFilename='dat/output_'+vTopicName[1:]+'_ICP_mod.dat'
+  myCreateDatFile(dataSeriesRaw,myTargetFilename,myTranslateX,myTranslateY,myRotate)
+  myGnuPlotCmds.append('"'+myTargetFilename+'" u 1:2 title "'+vTopicName[5:9]+'",')
+  #sample untranslated unrotated
+  myRotate=0#degrees rotate the robot angle, for plotting only
+  myTranslateX=0.0
+  myTranslateY=0.0
+  myTargetFilename='dat/output_'+vTopicName[1:]+'_ICP_sub.dat'
+  dataSeriesSub1=mySubtractBackground(dataSeriesRaw,dataSeriesBackGround)
+  dataSeriesSub=myFilterRange(dataSeriesSub1)
+  myCreateDatFile(dataSeriesSub,myTargetFilename,myTranslateX,myTranslateY,myRotate)
+  #2
+  myGnuPlotCmds.append('plot "'+myTargetFilename+'" u 1:2 lt rgb "red" title "'+vTopicName[5:9]+'"\n')
+  #Zoom in data range
+  myCentroid=myGetCentroid(dataSeriesSub)
+  myGnuPlotCmds.append('set xrange ['+str(myCentroid[0]-myZoom)+':'+str(myCentroid[0]+myZoom)+']\n')
+  myGnuPlotCmds.append('set yrange ['+str(myCentroid[1]-myZoom)+':'+str(myCentroid[1]+myZoom)+']\n')
+  #3 actual position
+  myGnuPlotCmds.append('plot "'+myTargetFilename+'" u 1:2 lt rgb "red" title "'+vTopicName[5:9]+' actual",')
+  #previous sample, green
+  myRotate=0#degrees rotate the robot angle, for plotting only
+  myTranslateX=0.0
+  myTranslateY=0.0
+  myTargetFilename='dat/output_'+vTopicName[1:]+'_ICP_prev.dat'
+  myCreateDatFileSample(dataSeriesPrevious,myTargetFilename,myTranslateX,myTranslateY,myRotate,dataSeriesBackGround)
+  #4
+  myGnuPlotCmds.append('"'+myTargetFilename+'" u 1:2 lt rgb "green" title "'+vTopicName[5:9]+' previous",')
+  if myDebug:print "myICP_Consecutive has "+str(len(myGnuPlotCmds))+" elements"
+  #ICP iterations, blue
+  myRotate=0#degrees rotate the robot angle, for plotting only
+  myTranslateX=0.0
+  myTranslateY=0.0
+  myTargetFilename='dat/output_'+vTopicName[1:]+'_ICP_iterations.dat'
+  myCreateDatFileICP(dataSeriesSub,myTargetFilename,myTranslateX,myTranslateY,myRotate,dataSeriesPrevious)
+  #5
+  myGnuPlotCmds.append('"'+myTargetFilename+'" lt rgb "blue" title "'+vTopicName[5:9]+' matches" with line\n')
+  if myDebug:print "myICP_Consecutive has "+str(len(myGnuPlotCmds))+" elements"
+  return myGnuPlotCmds
+  
+def myICP_Consecutive_MakeEmpty(vTopicName):
+  myGnuPlotCmds=[]
+  myTargetFilename='dat/output_'+vTopicName[1:]+'_ICP.dat'
+  f = open(myTargetFilename, 'w')
+  f.write("0 0\n")
+  f.close()
+  myGnuPlotCmds.append('plot "'+myTargetFilename+'" u 1:2 title "'+vTopicName[5:9]+'"\n')
+  myTargetFilename='dat/output_'+vTopicName[1:]+'_ICP_mod.dat'
+  f = open(myTargetFilename, 'w')
+  f.write("0 0\n")
+  f.close()
+  myGnuPlotCmds.append('"'+myTargetFilename+'" u 1:2 title "'+vTopicName[5:9]+'"')
+  return myGnuPlotCmds
+  
